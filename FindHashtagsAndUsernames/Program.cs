e8db5e03633d79313010ad1c3dbb7f8365b20b1c
turki-j <62262729+turki-j@users.noreply.github.com>
2021-04-15 12:02:06 +0300

    add project files

---
+++ FindHashtagsAndUsernames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindHashtagsAndUsernames
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
